478a4a1ca88f3ebbe4b80026813554b4fadf3ef5
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-02 22:42:58 +0530

    Final commit

--- src/components/App.js
+++ src/components/App.js
@@ -2,34 +2,58 @@ import React from 'react';
 import SearchBar from './SearchBar';
 import youtube from '../apis/youtube';
 import VideoList from './VideoList';
+import VideoDetail from './VideoDetail';
 
-class App extends React.Component{
+class App extends React.Component {
 
-    state={
-        videos:[]
+    componentDidMount(){
+        this.onTermSubmit('puppy videos');
+    }
+
+    state = {
+        videos: [],
+        selectedVideo: null
     };
 
-    onTermSubmit = async (term) =>{
-
-        const response = await youtube.get('/search',{
-            params:{
-                q:term
+    onTermSubmit = async (term) => {
+        const response = await youtube.get('/search', {
+            params: {
+                q: term
             }
         });
 
-        console.log(response.data.items);
+        // console.log(response.data.items);
         this.setState({
-            videos:response.data.items
+            videos: response.data.items,
+            selectedVideo:response.data.items[0]
         });
 
     };
 
-    render(){
-        return(
-            <div className="ui container"> 
-            <SearchBar onFormSubmit={this.onTermSubmit}/> 
-            <VideoList videos={this.state.videos}/>
-            
+    onVideoSelect = (video) => {
+        this.setState({
+            selectedVideo: video
+        });
+    }
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        </div>
+
+                    </div>
+
+                </div>
+
+
             </div>
         );
     }
--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import './VideoItem.css';
 
-const VideoItem= ({video}) =>{
+const VideoItem= ({video, onVideoSelect}) =>{
    // console.log(props);
     return (
-    <div className="video-item"> 
-        <img className="ui image" src={video.snippet.thumbnails.medium.url} />
+    <div onClick= {()=> onVideoSelect(video)} className="video-item"> 
+        <img alt={video.snippet.title} className="ui image" src={video.snippet.thumbnails.medium.url} />
         <div className="content">
             <div className="header">
             {video.snippet.title} 
