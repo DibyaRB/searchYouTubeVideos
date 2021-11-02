ef05f78854be9fc531872f4f764a13281cd53c28
Dibya Ranjan Bhoi <dibyabhoidibya@gmail.com>
2021-11-02 20:45:44 +0530

    Creating more components and adding API Calls

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,1 +1,13 @@
+import axios from 'axios';
+
 const KEY="AIzaSyDb7oBJznmHngJcn3GHy2Gv2oU8lCyBfw8";
+
+export default axios.create({
+    baseURL: 'https://www.googleapis.com/youtube/v3',
+    params:{
+        part:'snippet',
+        type:'video',
+        maxResults:10,
+        key:KEY
+    }
+});
